d8723b0c1fb36346cc61852f4f085c93d6a186b6
Daniel Mechanik <91424710+mechanik-daniel@users.noreply.github.com>
2023-06-04 08:26:19 +0300

    Update FumeError.ts

--- src/types/FumeError.ts
+++ src/types/FumeError.ts
@@ -1,1 +1,10 @@
+/**
+ * © Copyright Outburn Ltd. 2022-2023 All Rights Reserved
+ *   Project name: FUME
+ */
 
+import { JsonataError } from 'jsonata';
+
+export type FumeError = JsonataError & {
+  __isFumeError: true
+};
