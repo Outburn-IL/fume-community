2fd47827ac277bc33d3cf8f18e60cbef8cf15813
Daniel Mechanik <91424710+mechanik-daniel@users.noreply.github.com>
2023-06-04 08:32:16 +0300

    routes files

---
+++ serve/routes/root.js
@@ -0,0 +1,16 @@
+/**
+ * © Copyright Outburn Ltd. 2022-2023 All Rights Reserved
+ *   Project name: FUME
+ */
+
+import express from 'express';
+import rootControl from '../controllers/root';
+import { failOnStateless } from '../middleware/failOnStateless';
+
+const root = express.Router();
+
+root.get('/recache', failOnStateless, rootControl.recache);
+root.get('/', rootControl.get);
+root.post('/', rootControl.evaluate);
+
+export default root;
