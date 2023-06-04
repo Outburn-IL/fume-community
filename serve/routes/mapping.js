670702373990b6604336e77b6cdc18c3b9af5e17
Daniel Mechanik <91424710+mechanik-daniel@users.noreply.github.com>
2023-06-04 08:31:43 +0300

    Create mapping.js

---
+++ serve/routes/mapping.js
@@ -0,0 +1,16 @@
+/**
+ * © Copyright Outburn Ltd. 2022-2023 All Rights Reserved
+ *   Project name: FUME
+ */
+
+import express from 'express';
+import mappingControl from '../controllers/mapping';
+import { failOnStateless } from '../middleware/failOnStateless';
+
+const mapping = express.Router();
+
+mapping.use(failOnStateless);
+mapping.get('/:mappingId/', mappingControl.get);
+mapping.post('/:mappingId/', mappingControl.transform);
+
+export default mapping;
