2e098e0248777947238e586ce79636d9664be053
Daniel Mechanik <91424710+mechanik-daniel@users.noreply.github.com>
2024-02-28 16:39:07 +0200

    Restore mapping route (#15)
    * fixed mapping route
    
    * added tests for the mapping route
    
    * updated patch number to 1
    
    * replaced console with getLogger

--- src/controllers/mapping.ts
+++ src/controllers/mapping.ts
@@ -7,13 +7,14 @@ import { getCache } from '../helpers/cache';
 import { v2json } from '../helpers/hl7v2';
 import { parseCsv } from '../helpers/stringFunctions';
 import type { Request, Response } from 'express';
+import { getLogger } from '../helpers/logger';
 
 const get = async (req: Request, res: Response) => {
+  const logger = getLogger();
   try {
     const mappingId: string = req.params.mappingId;
-
     // get mapping expression from cache
-    const mappingObj = getCache().mappings.get(mappingId);
+    const mappingObj = getCache().compiledMappings.get(mappingId);
     const mapping: string = mappingObj.expression;
 
     if (mapping) {
@@ -23,26 +24,28 @@ const get = async (req: Request, res: Response) => {
       res.status(404).json({ message: 'not found' });
     }
   } catch (error) {
-    console.error({ error });
+    logger.error({ error });
     res.status(500).json({ message: error });
   }
 };
 
 const transform = async (req: Request, res: Response) => {
+  const logger = getLogger();
   try {
     const mappingId = req.params.mappingId;
-    const mappingFromCache = getCache().mappings[mappingId];
+    const mappingFromCache = getCache().compiledMappings.get(mappingId);
     const contentType = req.get('Content-Type');
     let inputJson;
 
     if (contentType?.startsWith('x-application/hl7-v2+er7')) {
-      console.log('Content-Type header suggests HL7 V2.x message');
+      logger.info('Content-Type header suggests HL7 V2.x message');
       const bodyString = req.body;
-      console.log('Trying to parse V2 message as JSON...');
+      logger.info('Trying to parse V2 message as JSON...');
       inputJson = await v2json(bodyString);
     } else {
       if (contentType?.startsWith('text/csv')) {
-        inputJson = parseCsv(req.body);
+        logger.info('Content-Type header suggests CSV input');
+        inputJson = await parseCsv(req.body);
       } else {
         inputJson = req.body;
       }
@@ -53,10 +56,11 @@ const transform = async (req: Request, res: Response) => {
       res.set('Content-Type', 'application/json');
       res.status(200).json(result);
     } else {
+      logger.error(`Mapping '${mappingId}' not found!`);
       res.status(404).json({ message: 'not found' });
     }
   } catch (error) {
-    console.error({ error });
+    logger.error({ error });
     res.status(500).json({ message: error });
   }
 };
