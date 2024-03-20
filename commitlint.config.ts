6a7da71f4f9b39c0b0718460831cd5ffbecffcf6
lizozom <lizka.k@gmail.com>
2024-03-20 11:30:22 +0200

    chore: added "breaking" rule type, chore triggers a patch release

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,9 +5,11 @@
 import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
-
   extends: ['@commitlint/config-conventional'],
-  formatter: '@commitlint/format'
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'breaking']]
+  }
 };
 
 export default Configuration;
