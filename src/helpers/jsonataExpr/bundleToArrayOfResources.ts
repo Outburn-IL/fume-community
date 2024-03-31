35f50303d3faf9d1ab0020e1c6fc8cc27a5f2300
Itay Shavit <itay@outburn.co.il>
2024-03-31 20:03:16 +0300

    fix: bundleToArrayOfResources

--- src/helpers/jsonataExpr/bundleToArrayOfResources.ts
+++ src/helpers/jsonataExpr/bundleToArrayOfResources.ts
@@ -1,10 +1,12 @@
 /**
- * © Copyright Outburn Ltd. 2022-2023 All Rights Reserved
- *   Project name: FUME
+ * © Copyright Outburn Ltd. 2022-2024 All Rights Reserved
+ *   Project name: FUME-COMMUNITY
  */
+
+import _ from 'lodash';
 
 // returns the diff between opening and closing parenthesis in  a single line
 export const bundleToArrayOfResources = (bundleArray): any => {
-  const arr = bundleArray.map(entry => entry?.entry[0]?.resource);
+  const arr = bundleArray.map(entry => _.get(entry, ['entry', '0', 'resource']));
   return !arr.filter(Boolean).length ? [] : arr;
 };
