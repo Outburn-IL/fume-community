8b226a9ffeef4fa6da5e716a83ce89a880510a8c
mechanik-daniel <daniel@outburn.co.il>
2024-12-07 01:04:46 +0200

    feat: new package installer

--- src/helpers/conformance/loadPackages.ts
+++ src/helpers/conformance/loadPackages.ts
@@ -2,95 +2,22 @@
  * © Copyright Outburn Ltd. 2022-2024 All Rights Reserved
  *   Project name: FUME-COMMUNITY
  */
-import axios from 'axios';
-import { fpl } from 'fhir-package-loader';
-import fs from 'fs-extra';
-import _ from 'lodash';
-import path from 'path';
 
 import { getLogger } from '../logger';
-import { getCachedPackageDirs, getCachePackagesPath } from './getCachePath';
+import ensurePack from './ensurePackageInstalled';
 
-export const loadPackage = async (fhirPackage: string | string[]) => {
+/**
+ * Download a list of packages from the registry to the local package cache (including dependencies)
+ * @param fhirPackage A string or array of strings in the format `packageId@version`
+ */
+export const downloadPackages = async (fhirPackage: string[]) => {
   try {
-    return await fpl(fhirPackage, {
-      log: (level: string, message: string) => {
-        if (level === 'error') {
-          getLogger().error(`FPL ${level}: ${message}`);
-        } else {
-          getLogger().info(`FPL ${level}: ${message}`);
-        }
-      }
-    });
+    for (const pack of fhirPackage) {
+      await ensurePack(pack);
+    };
+    return true;
   } catch (e) {
     getLogger().error(e);
     return null;
   }
-};
-
-export const loadPackages = async (packages: string[], mustPackages: string[], excludePackages: string[]) => {
-  const pkgToExclude = excludePackages;
-  // If there are packages without a vesion, retrieving it from fhir.org - creating a full list of packages to load.
-  const packagesToLoad = await Promise.all(packages.filter(p => !pkgToExclude.includes(p)).map(async p => {
-    if (p.split('@').length === 1) {
-      try {
-        const packageData = await axios.get(`https://packages.fhir.org/${p.split('@')[0]}/`);
-        const latestVersion = _.get(packageData, ['data', 'dist-tags', 'latest']);
-        return latestVersion ? `${p.split('@')}@${latestVersion}` : '';
-      } catch (e) {
-        getLogger().warn('Could not access - https://packages.fhir.org');
-        return '';
-      }
-    } else {
-      return p;
-    }
-  }));
-  mustPackages = _.uniq(mustPackages.concat(packagesToLoad.filter(Boolean)));
-  await loadPackage(packagesToLoad.filter(Boolean));
-
-  // Retrieving the list of the directories in the .fhir directory.
-  const cachePath = getCachePackagesPath();
-  const dirList: string[] = getCachedPackageDirs();
-
-  // For each directory, searching for it's package.json and getting it's dependencies for loading.
-  await Promise.all(dirList.map(async p => {
-    const currFilePath = path.join(cachePath, p, 'package\\package.json');
-    let currPackageFile;
-    try {
-      // Reading the file.
-      currPackageFile = await fs.readFile(currFilePath, 'utf8');
-    } catch (error) {
-      // If the package is a necessary one, and the reading fails - trying again.
-      if (mustPackages.includes(p)) {
-        await loadPackage([p]);
-        currPackageFile = await fs.readFile(currFilePath, 'utf8');
-      } else {
-        return;
-      }
-    }
-
-    try {
-      currPackageFile = JSON.parse(currPackageFile);
-      // If there are dependencies, and they haven't been loaded yet - load them.
-      if (_.get(currPackageFile, 'dependencies')) {
-        let currDependencies = Object.entries(_.get(currPackageFile, 'dependencies')).map(([key, value]) => {
-          if (!dirList.find(d => d.includes(key))) {
-            if (typeof value === 'string') return `${key}@${value}`;
-            return key;
-          } else {
-            return '';
-          }
-        });
-
-        if (currDependencies.filter(Boolean).length) {
-          currDependencies = currDependencies.filter(p => !pkgToExclude.includes(p));
-          mustPackages = _.uniq(mustPackages.concat(currDependencies.filter(Boolean)));
-          await loadPackage(currDependencies.filter(Boolean));
-
-          // For every dependency, load its dependencies :)
-          await loadPackages(currDependencies.filter(Boolean), mustPackages, excludePackages);
-        }
-      }
-    } catch (error) {}
-  }));
 };
