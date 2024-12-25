bc551ecbd24ed692d03e2a14485d48711e443488
mechanik-daniel <daniel@outburn.co.il>
2024-12-25 23:18:34 +0200

    test: enhance unit tests for $reference

--- src/helpers/fhirFunctions/reference.test.ts
+++ src/helpers/fhirFunctions/reference.test.ts
@@ -10,4 +10,7 @@ describe('reference', () => {
   test('returns reference on object with resourceType', async () => {
     expect(reference({ resourceType: 'Patient' })).toEqual('urn:uuid:b22de17e-2144-5f20-9fba-966d21409f94');
   });
+  test('returns undefined on undefined', async () => {
+    expect(reference(undefined)).toBeUndefined();
+  });
 });
