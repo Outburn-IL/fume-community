d8c1baa8decf044835098c00c18ac823cae0dfcf
mechanik-daniel <daniel@outburn.co.il>
2025-12-08 03:14:21 +0200

    refactor: remove unused throwParseError function

--- src/helpers/thrower.ts
+++ src/helpers/thrower.ts
@@ -3,17 +3,11 @@
  *   Project name: FUME-COMMUNITY
  */
 
-const throwParseError = (msg: string): undefined => {
-  const error = new Error(`FLASH Parse error: ${msg}`);
-  throw (error);
-};
-
 const throwRuntimeError = (msg: string): undefined => {
   const error = new Error(`Transformation error: ${msg}`);
   throw (error);
 };
 
 export default {
-  throwParseError,
   throwRuntimeError
 };
