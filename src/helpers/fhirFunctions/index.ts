d41d3a74165309b257239a70498758f8cdbef6ed
mechanik-daniel <daniel@outburn.co.il>
2025-12-13 23:53:43 +0200

    refactor: remove fhirVersionToMinor function and its tests

--- src/helpers/fhirFunctions/index.ts
+++ src/helpers/fhirFunctions/index.ts
@@ -2,12 +2,10 @@
  * © Copyright Outburn Ltd. 2022-2024 All Rights Reserved
  *   Project name: FUME-COMMUNITY
  */
-import { fhirVersionToMinor } from './fhirVersionToMinor';
 import { translateCode } from './translateCode';
 import { translateCoding } from './translateCoding';
 
 export default {
   translateCoding,
-  translateCode,
-  fhirVersionToMinor
+  translateCode
 };
