26954ac4ed5bb63fc04d7123b9b9925cb5edfbbe
mechanik-daniel <daniel@outburn.co.il>
2026-02-14 22:04:42 +0200

    chore: remove eslint-disable comments for global variables in type definitions

--- tests/global.d.ts
+++ tests/global.d.ts
@@ -9,14 +9,11 @@ import type { FumeServer } from '../src/server';
 declare global {
   // Set during tests/global setup
   // Accessed as globalThis.app, globalThis.fumeServer, etc.
-  // eslint-disable-next-line no-var
   var app: Application;
 
-  // eslint-disable-next-line no-var
   var fumeServer: FumeServer;
 
   // docker-compose services list (shape comes from docker-compose v2)
-  // eslint-disable-next-line no-var
   var services: Array<{
     name: string;
     command: string;
@@ -25,7 +22,6 @@ declare global {
   }>;
 
   // Optional: only used when enabling the mock Artifactory setup
-  // eslint-disable-next-line no-var
   var mockArtifactoryServer: unknown;
 }
 
